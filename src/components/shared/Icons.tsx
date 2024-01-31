93e650afe79b5812df43f651a9f6ef68c72d7bd7
RomaLetodiani <roman.letodiani03@gmail.com>
2024-01-31 12:56:40 +0400

    added icons

---
+++ src/components/shared/Icons.tsx
@@ -0,0 +1,17 @@
+export const MenuIcon = () => (
+  <svg width="40" height="17" xmlns="http://www.w3.org/2000/svg">
+    <g fill="#00001A" fillRule="evenodd">
+      <path d="M0 0h40v3H0zM0 7h40v3H0zM0 14h40v3H0z" />
+      <path d="M0 0h40v3H0z" />
+    </g>
+  </svg>
+);
+
+export const MenuIconClose = () => (
+  <svg width="32" height="31" xmlns="http://www.w3.org/2000/svg">
+    <g fill="#00001A" fillRule="evenodd">
+      <path d="m2.919.297 28.284 28.284-2.122 2.122L.797 2.419z" />
+      <path d="M.797 28.581 29.081.297l2.122 2.122L2.919 30.703z" />
+    </g>
+  </svg>
+);
